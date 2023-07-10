291100d422adbd77103d13121ab5464c95b2a54b
Álvaro Miranda <106751440+alvaroomiranda@users.noreply.github.com>
2023-07-10 12:33:54 -0300

    Add files via upload

---
+++ provaPratica/prova/index.js
@@ -0,0 +1,73 @@
+// ATIVIDADE 02:
+
+// const url = require('url')
+// const myUrl = new URL('https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf')
+
+// const objeto = {
+//     root: console.log('root:',''),
+//     dir: console.log('dir:',myUrl.origin),
+//     base: console.log('base:',myUrl.pathname),
+//     ext: console.log('ext:',myUrl.host),
+//     name: console.log('name:',myUrl.pathname),
+// }
+
+
+
+
+// ATIVIDADE 04:
+// const fs = require('fs')
+
+// const newFile = 'newFile.txt'
+
+// fs.rename('arquivo.txt', 'newFile.txt', (err) => {
+//     if (err) throw err;
+//     console.log('Rename complete!');
+//   }); 
+
+
+
+
+// ATIVIDADE 05:
+// const os = require('node:os'); 
+
+// console.log(os.cpus())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require('http')
+// const PORT = 5000
+
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200, 'Content-Type','text/plan')
+//     res.write('teste')
+//     res.end
+// })
+
+// server.listen(PORT,()=>{
+//     console.log('testando')
+// })
